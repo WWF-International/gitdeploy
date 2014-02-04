ec8517d84af7d1ed0dbca3a723d816bb0f7f2d8e
Paul Roberts <proberts@wwf.org.uk>
2014-02-04 10:53:46 +0000

    add config file and log file

--- gitdeploy.pl
+++ gitdeploy.pl
@@ -1,25 +1,80 @@
+#!/usr/bin/perl -w
+use feature qw(say);
 use strict;
+use YAML::Tiny;
+use Getopt::Std;
+use IO::Handle;
 
-my $pipe_fname = "/tmp/gitdeploy_test";
+my $CONFIG_FILE = "/etc/gitdeploy.yml";
 
-our $BASE_DIR = "$ENV{HOME}/git";
-# directories in which a `git pull` will be done
+# parse command-line options
+my %opts;
+getopts('df:', \%opts);
+if ($opts{f}) {
+	$CONFIG_FILE = $opts{'f'};
+}
 
-my @dirs = (
-	"$BASE_DIR/wwf-uk.earthhour-wordpress"
+# initialise to the default config
+my %CONF = (
+	pipef => "/tmp/gitdeploy_test",
+	logf  => "/var/log/gitdeploy.log",
+	repos => [] # empty set of repos by default
 );
 
-if (! -p $pipe_fname) { die "named pipe $pipe_fname doesn't exist"; }
+# attempt to read config file
+(-f $CONFIG_FILE) || die "Config file $CONFIG_FILE doesn't exist";
+my $fconf = YAML::Tiny->read($CONFIG_FILE);
+# only read the first document
+$fconf = $fconf->[0];
 
+# place the config into the %CONF var, leaving the defaults in for any key that
+# doesn't appear in the conf file
+for (keys %$fconf) {
+	$CONF{$_} = $fconf->{$_};
+}
+
+# open the log file
+open LOG, ">>", $CONF{logf} || die "Couldn't open logfile $CONF{logf} for writing";
+LOG->autoflush();
+
+sub logm {
+	print LOG "[", (scalar localtime), "] ", @_, "\n";
+}
+
+sub logdie {
+	logm @_;
+	die @_;
+}
+
+logm "Starting";
+
+# create the named pipe if it doesn't exist
+if (! -p $CONF{pipef}) {
+	logm "Creating named pipe $CONF{pipef}";
+	unlink $CONF{pipef} if -f $CONF{pipef};
+	require POSIX;
+	# don't forget umask(1) will filter the mode here ...
+	POSIX::mkfifo($CONF{pipef}, 0622) or
+		die("Could not create named pipe $CONF{pipef}");
+}
+
+# for the startup, log each configured repo; and make sure it exists
+for (@{$CONF{repos}}) {
+	logm "repo: $_";
+	logdie "ERROR: repo $_ doesn't exist" unless -d $_;
+}
+
+if ($opts{d}) { logm "debug exit"; exit };
+# now enter main loop
 while(1) {
-	open FH, "<", $pipe_fname || die "couldn't open";
+	logm "Waiting for wakeup call";
+	open FH, "<", $CONF{pipef} || die "couldn't open";
 	close FH;
-	print "Awake\n";
-	for (@dirs) {
-		die "Error: $_ not a directory" unless -d $_;
-		print "Updating: $_\n";
-		system "git --git-dir='$_' pull";
-		print "done\n";
+	logm "Awake\n";
+	for (@{$CONF{repos}}) {
+		logdie "Error: $_ not a directory" unless -d $_;
+		logm "Updating: $_\n";
+		system "git --git-dir='$_' pull -q";
+		logm "done\n";
 	}
-	print "Waiting...\n";
 }
